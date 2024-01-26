4f7c10d59610d52aad37e80aa64e557a9c27d4b3
pwolper <philip.wolper@gmail.com>
2024-01-26 19:06:10 +0100

    cleaned up debug

--- scimapai.py
+++ scimapai.py
@@ -147,7 +147,6 @@ st.sidebar.markdown("After pasting your OpenAI API key, paste any scientific tex
 st.sidebar.markdown("**Contact**")
 st.sidebar.markdown("Created by Philip Wolper [phi.wolper@gmail.com](phi.wolper@gmail.com). Code is available on [GitHub](https://github.com/pwolper/scimapai.git) here. Feeback is very welcome.")
 st.sidebar.markdown("**Knowledge Graph Options**")
-debug=st.sidebar.checkbox("Show debugging information")
 
 get_api_key()
 get_text()
@@ -162,6 +161,7 @@ if "text_input" in st.session_state:
         st.markdown("**Knowledge Graph:**")
         components.html(source_code, height=550,width=1350)
         download=st.download_button("Download HTML", data=source_code, file_name="knowledge_graph.html")
+         debug=st.sidebar.checkbox("Show debugging information")
 
         st.markdown("**Summary:**")
         summary = llm_summary_call(st.session_state.text_input, st.session_state.openai_api_key)
